14dcb7bb1e5aae56cd78c5e21bb4cc478375a75b
hemh <919052316@qq.com>
2019-10-29 13:56:34 +0800

    proxy-filter-sort change commit

--- libpeony-qt/model/file-item-proxy-filter-sort-model.h
+++ libpeony-qt/model/file-item-proxy-filter-sort-model.h
@@ -15,9 +15,45 @@ class PEONYCORESHARED_EXPORT FileItemProxyFilterSortModel : public QSortFilterPr
 {
     Q_OBJECT
 public:
+    enum FilterFileType{
+        ALL_TYPE,
+        FILE_FOLDER,
+        PICTURE,
+        VIDEO,
+        TXT_FILE,
+        AUDIO,
+        OTHERS
+    };
+    Q_ENUM(FilterFileType)
+    enum FilterFileModifyTime{
+        ALL_TIME,
+        TODAY,
+        THIS_WEEK,
+        THIS_MONTH,
+        THIS_YEAR,
+        YEAR_AGO
+    };
+    Q_ENUM(FilterFileModifyTime)
+    enum FilterFileSize{
+        ALL_SIZE,
+        TINY,
+        SMALL,
+        MEDIUM,
+        BIG,
+        LARGE
+    };
+    Q_ENUM(FilterFileSize)
+
+    const QString Folder_Type = "inode/directory";
+    const QString Image_Type = "image/";
+    const QString Video_Type = "video/";
+    const QString Text_Type = "text/";
+    const QString Audio_Type = "audio/";
+
     explicit FileItemProxyFilterSortModel(QObject *parent = nullptr);
     void setSourceModel(QAbstractItemModel *model) override;
     void setShowHidden(bool showHidden);
+    void setFilterConditions(int fileType=0, int modifyTime=0, int fileSize=0);
 
     FileItem *itemFromIndex(const QModelIndex &proxyIndex);
     QModelIndex getSourceIndex(const QModelIndex &proxyIndex);
@@ -35,9 +71,15 @@ protected:
 
 private:
     bool startWithChinese(const QString &displayName) const;
+    bool checkFileTypeFilter(QString type) const;
+    bool checkFileModifyTimeFilter(QString modifiedDate) const;
+    bool checkFileSizeFilter(quint64 size) const;
 
 private:
     bool m_show_hidden = false;
+    const int ALL_FILE = 0;
+    const quint64 K_BASE = 1000;
+    int m_show_file_type=ALL_FILE, m_show_modify_time=ALL_FILE, m_show_file_size=ALL_FILE;
 };
 
 }
