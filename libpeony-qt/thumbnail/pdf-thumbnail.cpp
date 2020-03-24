d58bf9ec31032d49857d4e957cf38f303da5cced
MeganHemh <919052316@qq.com>
2020-03-24 11:15:46 +0800

    Comment throw to return to fix crash issue when preview pdf file.

--- libpeony-qt/thumbnail/pdf-thumbnail.cpp
+++ libpeony-qt/thumbnail/pdf-thumbnail.cpp
@@ -43,15 +43,19 @@ PdfThumbnail::~PdfThumbnail() {
 QPixmap PdfThumbnail::generateThumbnail(unsigned int pageNum) {
     try {
         if (this->documentPrivate == nullptr)
-            throw "pdf document not existed";
+            //throw "pdf document not existed";
+            //fix crash issue, change throw to return
+            return QPixmap();
         pagePrivate = documentPrivate->page(pageNum);
         if (pagePrivate == nullptr)
-            throw "load pdf page failed";
+            //throw "load pdf page failed";
+            return QPixmap();
         auto image = pagePrivate->renderToImage(
                          72 * 2, 72 * 2, 0, 0, pagePrivate->pageSizeF().width() * 2,
                          pagePrivate->pageSizeF().height() * 2);
         if (image.isNull())
-            throw "load pdf page image failed";
+            //throw "load pdf page image failed";
+            return QPixmap();
         return QPixmap::fromImage(image);
     } catch (char *e) {
         qDebug() << e;
