3fe835798e9c697df96b6068daf1fbba0fcc7bcc
MeganHemh <919052316@qq.com>
2020-06-28 11:35:14 +0800

    Fix copyright issue.

--- libpeony-qt/controls/tool-bar/search-bar-container.cpp
+++ libpeony-qt/controls/tool-bar/search-bar-container.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * Authors: Yue Lan <lanyue@kylinos.cn>
+ * Authors: MeihongHe <hemeihong@kylinos.cn>
  *
  */
 
