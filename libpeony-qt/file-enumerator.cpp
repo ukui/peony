ea7518369cce20aba13fb2f259af2d0d31b7db0e
jun <zhangjun@kylinos.cn>
2020-07-17 09:36:31 +0800

    solve the mouse WaitCursor while enter file no permission denied

--- libpeony-qt/file-enumerator.cpp
+++ libpeony-qt/file-enumerator.cpp
@@ -282,6 +282,9 @@ void FileEnumerator::handleError(GError *err)
     case G_IO_ERROR_PERMISSION_DENIED:
         //FIXME: do i need add an auth function for this kind of errors?
         QMessageBox::critical(nullptr, tr("Error"), err->message);
+        //send enumerateFinished SIGNALS,solve the mouse WaitCursor
+        enumerateFinished(true);
+
         break;
     case G_IO_ERROR_NOT_FOUND:
         Q_EMIT prepared(GErrorWrapper::wrapFrom(g_error_new(G_IO_ERROR, G_IO_ERROR_NOT_FOUND, "file not found")));
