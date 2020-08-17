6cf60526438e54bf0eb3a448032cfdadfa026833
Yue-Lan <lanyue@ubuntukylin.com>
2020-08-17 11:56:00 +0800

    NavigationTabBar::relayoutFloatButton: fix the buttons overriding while there are too many tabs.

--- src/control/navigation-tab-bar.cpp
+++ src/control/navigation-tab-bar.cpp
@@ -179,8 +179,13 @@ void NavigationTabBar::relayoutFloatButton(bool insterted)
     //qDebug()<<"relayout";
     auto lastTabRect = tabRect(count() - 1);
     fixedY = lastTabRect.center().y() - m_float_button->height()/2;
-    m_float_button->move(lastTabRect.right(), fixedY);
+    int fixedX = qMin(this->width() - qApp->style()->pixelMetric(QStyle::PM_TabBarScrollButtonWidth)*2 - m_float_button->width(), lastTabRect.right());
+    if (count() == 1) {
+        fixedX = lastTabRect.right();
+    }
+    m_float_button->move(fixedX, fixedY);
     setFixedHeight(lastTabRect.height());
+    m_float_button->raise();
 }
 
 void NavigationTabBar::dragEnterEvent(QDragEnterEvent *e)
