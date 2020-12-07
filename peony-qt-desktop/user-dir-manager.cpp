64dddda70256c8f51627fb4302179afe17f37541
zhangpengfei <zhangpengfei@kylinos.com>
2020-12-07 10:07:27 +0800

    [FIX] The disable thumbnail option does not work on the desktop.

--- peony-qt-desktop/user-dir-manager.cpp
+++ peony-qt-desktop/user-dir-manager.cpp
@@ -21,22 +21,42 @@ UserdirManager::UserdirManager(QObject *parent) : QObject(parent)
     pwd=getpwuid(getuid());
     auto userName = pwd->pw_name;
     m_user_name = QString(userName);
+
+    m_settings = new QSettings("org.ukui", "peony-qt-preferences", this);
+    m_do_not_thumbnail = m_settings->value("do-not-thumbnail").toBool();
     m_user_dir_watcher = new QFileSystemWatcher(this);
 
-    QString path = QString("/home/"+m_user_name+"/.config/user-dirs.dirs");
-    m_user_dir_watcher->addPath(path);
+    QString path0 = QString("/home/"+m_user_name+"/.config/user-dirs.dirs");
+    QString path1 = QString("/home/"+m_user_name+"/.config/org.ukui/peony-qt-preferences.conf");
+
+    m_user_dir_watcher->addPath(path0);
+    m_user_dir_watcher->addPath(path1);
 
     getUserdir();
 
     connect(m_user_dir_watcher, &QFileSystemWatcher::fileChanged, [=](const QString &uri){
-        getUserdir();
-        moveFile();
-        m_user_dir_watcher->addPath(path);
+        //user-dirs.dirs
+        if(uri == path0)
+        {
+            getUserdir();
+            moveFile();
+        }
+        //peony-qt-preferences  only thumbnail setting for now.
+        else if(uri == path1)
+        {
+            m_settings = new QSettings("org.ukui", "peony-qt-preferences", this);
+            if(m_do_not_thumbnail != m_settings->value("do-not-thumbnail").toBool())
+            {
+                m_do_not_thumbnail = m_settings->value("do-not-thumbnail").toBool();
+                Q_EMIT thumbnailSetingChange();
+            }
+        }
+        m_user_dir_watcher->addPath(uri);
 
     });
 
 }
-
+// read user-dirs.dirs for all XDG standard path.
 void UserdirManager::getUserdir()
 {
     if(!m_current_user_dir.isEmpty())
@@ -57,9 +77,11 @@ void UserdirManager::getUserdir()
     m_current_user_dir.insert("XDG_PICTURES_DIR",settings->value(QString("XDG_PICTURES_DIR")).toString().replace("$HOME","/home/"+m_user_name) + "/");
     m_current_user_dir.insert("XDG_VIDEOS_DIR",settings->value(QString("XDG_VIDEOS_DIR")).toString().replace("$HOME","/home/"+m_user_name) + "/");
 
+    //XDG_TEMPLATES_DIR will be used by right click menu.
     GlobalSettings::getInstance()->setValue(TEMPLATES_DIR,m_current_user_dir.value("XDG_TEMPLATES_DIR"));
 }
 
+//rename the old paths to overwrire new one.
 void UserdirManager::moveFile()
 {
     QMap<QString, QString>::const_iterator i;
