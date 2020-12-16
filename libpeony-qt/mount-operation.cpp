0217f07f4b929129867eb6c5b98d4dc9ef9ba578
Yue-Lan <lanyue@ubuntukylin.com>
2020-12-16 09:52:11 +0800

    fix unexpected mount loop for anoymous.
    link to: http://172.17.66.192/biz/bug-view-19304.html

--- libpeony-qt/mount-operation.cpp
+++ libpeony-qt/mount-operation.cpp
@@ -153,6 +153,14 @@ void MountOperation::ask_password_cb(GMountOperation *op,
     Q_UNUSED(flags);
     Q_UNUSED(p_this);
 
+    if (g_mount_operation_get_anonymous(op)) {
+        // need passwd, should restart a mount operation?
+        auto err = GErrorWrapper::wrapFrom(g_error_new(G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED, message));
+        p_this->finished(err);
+
+        g_mount_operation_reply (op, G_MOUNT_OPERATION_UNHANDLED);
+    }
+
     g_mount_operation_reply (op, G_MOUNT_OPERATION_HANDLED);
 }
 
