56a49926a4a23d5cdbc1cef24cedc5f95c53ea64
yhz0120 <943588990@qq.com>
2020-12-21 18:01:58 +0800

    Fix two known problems   1.sidebar eject-button show problem.   2.after close gparted,some of the partition names are confusing.

--- libpeony-qt/model/side-bar-model.cpp
+++ libpeony-qt/model/side-bar-model.cpp
@@ -203,14 +203,21 @@ QVariant SideBarModel::data(const QModelIndex &index, int role) const
 
     SideBarAbstractItem *item = static_cast<SideBarAbstractItem*>(index.internalPointer());
     if (index.column() == 1) {
-        if (role == Qt::DecorationRole && item->isRemoveable())
-            if (item->isMounted())
+        if(role == Qt::DecorationRole){
+            bool unmountAble,ejectAble;
+            unmountAble = item->isMountable();
+            ejectAble = item->isEjectable();
+            if(unmountAble && ejectAble)
                 return QVariant(QIcon::fromTheme("media-eject"));
-            else//if volume has been unmounted,doesn't show icon.
+            else if(unmountAble){
+                if(item->isMounted())
+                    return QVariant(QIcon::fromTheme("media-eject"));
+                else
+                    return QVariant();
+            }else
                 return QVariant();
-        else {
+        }else
             return QVariant();
-        }
     }
 
     switch (role) {
