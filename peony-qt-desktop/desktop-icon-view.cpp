0681e0c26dc0b4a46041bfbe7a2dc942c9b9cbfa
Yue-Lan <lanyue@ubuntukylin.com>
2021-04-19 14:58:57 +0800

    fix unexpected item relayout. link to:#47793

--- peony-qt-desktop/desktop-icon-view.cpp
+++ peony-qt-desktop/desktop-icon-view.cpp
@@ -205,7 +205,7 @@ DesktopIconView::DesktopIconView(QWidget *parent) : QListView(parent)
 
     m_proxy_model->setSourceModel(m_model);
 
-    connect(m_model, &QAbstractItemModel::rowsRemoved, this, [=](){
+    connect(m_proxy_model, &QAbstractItemModel::rowsRemoved, this, [=](){
         auto itemsNeedRelayout = m_model->m_items_need_relayout;
         if (!itemsNeedRelayout.isEmpty()) {
             this->relayoutExsitingItems(itemsNeedRelayout);
