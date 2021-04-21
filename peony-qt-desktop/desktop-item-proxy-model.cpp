b08a90d3abec41b6da44417225dc372935329cb0
MeihongHe <hemeihong@kylinos.cn>
2021-04-21 15:54:13 +0800

    Fix Desktop folder in desktop not show issue. Link to bug#49131.

--- peony-qt-desktop/desktop-item-proxy-model.cpp
+++ peony-qt-desktop/desktop-item-proxy-model.cpp
@@ -93,7 +93,10 @@ bool DesktopItemProxyModel::filterAcceptsRow(int source_row, const QModelIndex &
     //fix desktop show Desktop folder issue, bug#20293
     if (QUrl(uri).path() == QStandardPaths::writableLocation(QStandardPaths::DesktopLocation) + "/Desktop"
         || QUrl(uri).path() == QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Desktop" + "/Desktop")
-        return false;
+    {
+        if (! QFile::exists(QUrl(uri).path()))
+           return false;
+    }
 
     if (info->isDesktopFile() && nullptr != info->desktopName()){
         if (m_bwListInfo->isBlackListMode()){
