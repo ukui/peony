ukui/peony

commit 0681e0c26dc0b4a46041bfbe7a2dc942c9b9cbfa

fix unexpected item relayout. link to:#47793

peony-qt-desktop/desktop-icon-view.cpp
@@ -205,7 +205,7 @@ DesktopIconView::DesktopIconView(QWidget *parent) : QListView(parent)
 
     m_proxy_model->setSourceModel(m_model);
 
-    connect(m_model, &QAbstractItemModel::rowsRemoved, this, [=](){
+    connect(m_proxy_model, &QAbstractItemModel::rowsRemoved, this, [=](){
         auto itemsNeedRelayout = m_model->m_items_need_relayout;
         if (!itemsNeedRelayout.isEmpty()) {
             this->relayoutExsitingItems(itemsNeedRelayout);

commit ddd2f001cac7c6d5e42a2866e2b080a2ac288809

fix items resorted while file trashed/deleted, this bug is caused by 0681e0c26dc0b4a46041bfbe7a2dc942c9b9cbfa

peony-qt-desktop/desktop-icon-view.cpp
@@ -205,6 +205,14 @@ DesktopIconView::DesktopIconView(QWidget *parent) : QListView(parent)
 
     m_proxy_model->setSourceModel(m_model);
 
+    connect(m_model, &QAbstractItemModel::rowsRemoved, this, [=](){
+        for (auto uri : getAllFileUris()) {
+            auto pos = getFileMetaInfoPos(uri);
+            if (pos.x() >= 0)
+                updateItemPosByUri(uri, pos);
+        }
+    });
+
     connect(m_proxy_model, &QAbstractItemModel::rowsRemoved, this, [=](){
         auto itemsNeedRelayout = m_model->m_items_need_relayout;
         if (!itemsNeedRelayout.isEmpty()) {