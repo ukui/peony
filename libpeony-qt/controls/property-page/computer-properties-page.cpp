a631ee1cd604ec02d1322da89da64a7bbf48fe9f
MeihongHe <hemeihong@kylinos.cn>
2021-04-29 15:07:22 +0800

    Fix data volume calculate capacity wrong issue. Link to bug#51957.

--- libpeony-qt/controls/property-page/computer-properties-page.cpp
+++ libpeony-qt/controls/property-page/computer-properties-page.cpp
@@ -168,29 +168,31 @@ ComputerPropertiesPage::ComputerPropertiesPage(const QString &uri, QWidget *pare
             char *aviliable_format = strtok(g_format_size_full(aviliable,G_FORMAT_SIZE_IEC_UNITS),"iB");
 
             //fix system Udisk calculate size wrong issue
-            QString m_volume_name, m_unix_device, m_display_name, sizeInfo;
-            FileUtils::queryVolumeInfo(uri, m_volume_name, m_unix_device, m_display_name);
-            bool bMobileDevice = false;
-            //U disk or other mobile device
-            if (! m_unix_device.isEmpty() && ! uri.startsWith("computer:///WDC"))
-            {
-               char dev_name[256] ={0};
-               strncpy(dev_name, m_unix_device.toUtf8().constData(),sizeof(m_unix_device.toUtf8().constData()-1));
-               auto size = FileUtils::getDeviceSize(dev_name);
-               if (size > 0)
-               {
-                   sizeInfo = QString::number(size, 'f', 1);
-                   qDebug() << "size:" <<size;
-                   sizeInfo += "G";
-                   bMobileDevice = true;
-               }
-            }
+            //comment to fix caculate volume capacity wrong issue,bug#51957
+//            QString m_volume_name, m_unix_device, m_display_name, sizeInfo;
+//            FileUtils::queryVolumeInfo(uri, m_volume_name, m_unix_device, m_display_name);
+//            bool bMobileDevice = false;
+//            //U disk or other mobile device
+//            if (! m_unix_device.isEmpty() && ! uri.startsWith("computer:///WDC"))
+//            {
+//               char dev_name[256] ={0};
+//               strncpy(dev_name, m_unix_device.toUtf8().constData(),sizeof(m_unix_device.toUtf8().constData()-1));
+//               auto size = FileUtils::getDeviceSize(dev_name);
+//               if (size > 0)
+//               {
+//                   sizeInfo = QString::number(size, 'f', 1);
+//                   qDebug() << "size:" <<size;
+//                   sizeInfo += "G";
+//                   bMobileDevice = true;
+//               }
+//            }
 
             char *fs_type = g_file_info_get_attribute_as_string(info, G_FILE_ATTRIBUTE_FILESYSTEM_TYPE);
             m_layout->addRow(tr("Name: "), new QLabel(mount->name(), this));
-            if (bMobileDevice)
+/*            if (bMobileDevice)
                 m_layout->addRow(tr("Total Space: "), new QLabel(sizeInfo, this));
-            else if (total != 0) {
+            else */
+            if (total != 0) {
                 m_layout->addRow(tr("Total Space: "), new QLabel(total_format, this));
             }
             if (used != 0) {
