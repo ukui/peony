ea2b799f59040ea07dc31a4e9a29b7b92a892925
dingjingmaster <dingjing@live.cn>
2021-06-10 14:28:21 +0800

    [FIX]  * 字体调大，错误处理弹框界面无法显示全字体 [LINK] 60759

--- libpeony-qt/file-operation/file-operation-error-dialogs.cpp
+++ libpeony-qt/file-operation/file-operation-error-dialogs.cpp
@@ -253,21 +253,14 @@ void Peony::FileOperationErrorDialogWarning::handle(Peony::FileOperationError &e
 {
     m_error = &error;
 
+    QStyleOptionViewItem opt;
     if (nullptr != m_error->errorStr) {
-        QString htmlString = QString("<style>"
-                                     "  p{font-size:14px;line-height:100%;}"
-                                     "  .bold{text-align: left;font-size:13px;font-wight:500;}"
-                                     "</style>"
-                                     "<p class='bold'>%1</p>")
-                .arg(m_error->errorStr.toHtmlEscaped());
+        QString htmlString = QString("<p>%1</p>")
+                .arg(opt.fontMetrics.elidedText(m_error->errorStr.toHtmlEscaped(), Qt::ElideMiddle, 480).toHtmlEscaped());
         m_text->setText(htmlString);
     } else {
-        QString htmlString = QString("<style>"
-                                     "  p{font-size:14px;line-height:100%;}"
-                                     "  .bold{text-align: left;font-size:13px;font-wight:500;}"
-                                     "</style>"
-                                     "<p>%1</p>")
-                .arg(tr("Make sure the disk is not full or write protected and that the file is not protected"));
+        QString htmlString = QString("<p>%1</p>")
+                .arg(opt.fontMetrics.elidedText(tr("Make sure the disk is not full or write protected and that the file is not protected"), Qt::ElideMiddle, 480).toHtmlEscaped());
         m_text->setText(htmlString);
     }
 
@@ -377,21 +370,14 @@ void Peony::FileOperationErrorDialogNotSupported::handle(Peony::FileOperationErr
 {
     m_error = &error;
 
+    QStyleOptionViewItem opt;
     if (nullptr != m_error->errorStr) {
-        QString htmlString = QString("<style>"
-                                     "  p{font-size:14px;line-height:100%;}"
-                                     "  .bold{text-align: left;font-size:13px;font-wight:500;}"
-                                     "</style>"
-                                     "<p class='bold'>%1</p>")
-                .arg(m_error->errorStr.toHtmlEscaped());
+        QString htmlString = QString("<p>%1</p>")
+                .arg(opt.fontMetrics.elidedText(m_error->errorStr.toHtmlEscaped(), Qt::ElideMiddle, 480).toHtmlEscaped());
         m_text->setText(htmlString);
     } else {
-        QString htmlString = QString("<style>"
-                                     "  p{font-size:14px;line-height:100%;}"
-                                     "  .bold{text-align: left;font-size:13px;font-wight:500;}"
-                                     "</style>"
-                                     "<p>%1</p>")
-                .arg(tr("Make sure the disk is not full or write protected and that the file is not protected"));
+        QString htmlString = QString("<p>%1</p>")
+                .arg(opt.fontMetrics.elidedText(tr("Make sure the disk is not full or write protected and that the file is not protected"), Qt::ElideMiddle, 480).toHtmlEscaped());
         m_text->setText(htmlString);
     }
 
