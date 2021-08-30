2a5582503466f0d814153dc7a8977ab3fa5968ce
hemh <hemeihong@kylinos.cn>
2021-08-30 10:11:49 +0800

    Fix change file content and sort not update issue, link to bug#76908.

--- peony-qt-desktop/desktops/desktop/model/desktop-item-model.cpp
+++ peony-qt-desktop/desktops/desktop/model/desktop-item-model.cpp
@@ -284,6 +284,12 @@ DesktopItemModel::DesktopItemModel(DesktopIconView *view, QObject *parent)
             Q_EMIT this->requestLayoutNewItem(info->uri());
             Q_EMIT this->fileCreated(uri);
         }
+        else{
+            //file content changed, need update fileinfo, fix bug#76908
+            auto job = new FileInfoJob(info);
+            job->setAutoDelete();
+            job->querySync();
+        }
     });
 
     m_desktop_watcher->connect(m_desktop_watcher.get(), &FileWatcher::fileDeleted, [=](const QString &uri) {
