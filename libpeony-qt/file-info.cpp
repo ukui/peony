83be1de2384f2498032ee0b8d6a6cf8e4c57e60d
hemh <hemeihong@kylinos.cn>
2021-08-31 14:22:33 +0800

    Fix soft link use relative path and access wrong issue, link to bug#73529.

--- libpeony-qt/file-info.cpp
+++ libpeony-qt/file-info.cpp
@@ -198,6 +198,13 @@ const QString FileInfo::symlinkTarget()
 {
     if (m_symlink_target == ".")
         return "";
+
+    //fix soft link use relative path issue, link to bug#73529
+    if (! m_symlink_target.startsWith("/"))
+    {
+        QString parentUri = FileUtils::getParentUri(m_uri);
+        m_symlink_target = QUrl(parentUri).path() + "/" + m_symlink_target;
+    }
     return m_symlink_target;
 }
 
