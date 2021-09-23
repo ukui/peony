e68385231db871f4e7581a7b9e0b76294f671c5f
erguangqiang <erguangqiang@kylinos.cn>
2021-09-23 17:18:56 +0800

    KYLIN: fix bug 80956, cannot mark file in filesafe.
      - 修复Bug80956，文件保护箱下文件和目录不能正常标记颜色；
    
    Signed-off-by: erguangqiang <erguangqiang@kylinos.cn>

--- libpeony-qt/file-meta-info.cpp
+++ libpeony-qt/file-meta-info.cpp
@@ -79,44 +79,43 @@ void FileMetaInfo::setMetaInfoStringList(const QString &key, const QStringList &
 
 void FileMetaInfo::setMetaInfoVariant(const QString &key, const QVariant &value, bool syncToFile)
 {
-    //    if (!m_mutex.tryLock(300)) {
-    //        return;
-    //    }
+//    if (!m_mutex.tryLock(300)) {
+//        return;
+//    }
 
-        QString realKey = key;
-        if (!key.startsWith("metadata::"))
-            realKey = "metadata::" + key;
+    QString realKey = key;
+    if (!key.startsWith("metadata::"))
+        realKey = "metadata::" + key;
 
-        m_meta_hash.remove(realKey);
-        m_meta_hash.insert(realKey, value);
-        GFile *file = g_file_new_for_uri(m_uri.toUtf8().constData());
-    //    GFileInfo *info = g_file_info_new();
-        std::string tmp = realKey.toStdString();
-        auto data = value.toString().toUtf8().data();
-    //    g_file_info_set_attribute(info, tmp.c_str(), G_FILE_ATTRIBUTE_TYPE_STRING, (gpointer)data);
-        //FIXME: should i add callback?
-    //    g_file_set_attributes_async(file,
-    //                                info,
-    //                                G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
-    //                                0,
-    //                                nullptr,
-    //                                nullptr,
-    //                                nullptr);
-        //qDebug()<<tmp.c_str()<<value;
-        if (syncToFile) {
-            GError *err = nullptr;
-            g_file_set_attribute(file, tmp.c_str(), G_FILE_ATTRIBUTE_TYPE_STRING, (gpointer)data,
-                                 G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, nullptr, &err);
+    m_meta_hash.remove(realKey);
+    m_meta_hash.insert(realKey, value);
+    GFile *file = g_file_new_for_uri(m_uri.toUtf8().constData());
+//    GFileInfo *info = g_file_info_new();
+//    g_file_info_set_attribute(info, tmp.c_str(), G_FILE_ATTRIBUTE_TYPE_STRING, (gpointer)data);
+    //FIXME: should i add callback?
+//    g_file_set_attributes_async(file,
+//                                info,
+//                                G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
+//                                0,
+//                                nullptr,
+//                                nullptr,
+//                                nullptr);
+    //qDebug()<<tmp.c_str()<<value;
+    if (syncToFile) {
+        GError *err = nullptr;
+        g_file_set_attribute(file, realKey.toLatin1().data(), G_FILE_ATTRIBUTE_TYPE_STRING,
+                             (gpointer)value.toString().toUtf8().data(),
+                             G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, nullptr, &err);
 
-            if (err) {
-                qDebug()<<err->message;
-                g_error_free(err);
-            }
+        if (err) {
+            qDebug()<<err->message;
+            g_error_free(err);
         }
+    }
 
-    //    g_object_unref(info);
-        g_object_unref(file);
-    //    m_mutex.unlock()
+//    g_object_unref(info);
+    g_object_unref(file);
+//    m_mutex.unlock();
 }
 
 const QVariant FileMetaInfo::getMetaInfoVariant(const QString &key)
