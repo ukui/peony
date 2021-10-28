26b288f6185e2a8e62be8d2a196f7af91e52bfbc
yangling <yangling@kylinos.cn>
2021-10-28 10:18:41 +0800

    Optimize directory jump when unmounting.

--- libpeony-qt/file-watcher.cpp
+++ libpeony-qt/file-watcher.cpp
@@ -129,8 +129,9 @@ void FileWatcher::startMonitor()
         }
     });
     connect(Experimental_Peony::VolumeManager::getInstance(), &Experimental_Peony::VolumeManager::signal_unmountFinished, this, [=](const QString &uri){
-        /* volume卸载完成跳转到计算机目录 */
-        if (m_uri.contains(uri)||m_target_uri.contains(uri)) {
+        /* volume卸载完成跳转到计算机目录(只有进入volume内部目录，卸载时才跳转，空光盘比较特殊) */
+        auto fixedUri = uri;
+        if (m_uri == uri || m_target_uri == uri || m_uri + "/" == fixedUri||m_uri=="burn:///") {
             Q_EMIT directoryUnmounted("computer:///");
         }
     });
