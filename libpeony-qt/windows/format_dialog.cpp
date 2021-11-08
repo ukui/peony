b638513820a457e3b66b9921602eb01f69eaaaa2
Yue-Lan <lanyue@ubuntukylin.com>
2021-11-08 19:28:44 +0800

    Revert "[FIX]  格式化过程中进度条为等待状态，需要同步到 9A0"
    This reverts commit b053138796cad30291c04afb2285e0bf29a25642.

--- libpeony-qt/windows/format_dialog.cpp
+++ libpeony-qt/windows/format_dialog.cpp
@@ -34,8 +34,6 @@
 #include <QMessageBox>
 #include <KWindowSystem>
 #include <QCloseEvent>
-#include <QStyleOption>
-#include <QPainter>
 
 using namespace  Peony;
 static bool b_finished = false;
@@ -104,12 +102,8 @@ Format_Dialog::Format_Dialog(const QString &m_uris,SideBarAbstractItem *m_item,Q
             ui->lineEdit_device_name->setText(LinuxPWDHelper::getCurrentUser().fullName());
        }
 
-       ui->label_process->setHidden (true);
        ui->progressBar_process->setValue(0);
-       ui->progressBar_process->setStyleSheet ("");
-       ui->progressBar_process->setFixedHeight (16);
-       ui->progressBar_process->setFixedWidth (400);
-       ui->progressBar_process->setStyle (new ProgressBarStyle);
+
 
        connect(ui->pushButton_ok, SIGNAL(clicked(bool)), this, SLOT(acceptFormat(bool)));
 
@@ -269,8 +263,7 @@ void Format_Dialog::acceptFormat(bool)
     //while get ensure emit , do format 
     connect(this,&Format_Dialog::ensure_format,[=](bool){
         // time begin loop
-//        my_time->start();
-        ui->progressBar_process->setRange (0, 0);
+        my_time->start();
 
         // set ui button disable
         ui->pushButton_ok->setDisabled(TRUE);
@@ -590,16 +583,12 @@ void Format_Dialog::format_ok_dialog()
         QMessageBox::about(m_parent,QObject::tr("format"),QObject::tr("Formatting successful! But failed to set the device name."));
     }
 
-    ui->progressBar_process->setRange (0, 100);
-    ui->progressBar_process->setValue (100);
     ui->pushButton_close->setEnabled(true);
 }
 
 
 void Format_Dialog::format_err_dialog()
 {
-    ui->progressBar_process->setRange (0, 100);
-    ui->progressBar_process->setValue (0);
       QMessageBox::warning(m_parent,QObject::tr("qmesg_notify"),QObject::tr("Sorry, the format operation is failed!"));
       ui->pushButton_close->setEnabled(true);
 }
@@ -808,217 +797,4 @@ void Format_Dialog::closeEvent(QCloseEvent *e)
         e->ignore();
         return;
     }
-}
-
-void ProgressBarStyle::polish(QWidget *widget)
-{
-    if (QProgressBar* p = qobject_cast<QProgressBar*>(widget)) {
-        connect (mAnimation, &QVariantAnimation::valueChanged, widget, [=] (const QVariant&) {
-                p->update ();
-            }, Qt::UniqueConnection);
-    }
-}
-
-int ProgressBarStyle::pixelMetric(PixelMetric m, const QStyleOption *opt, const QWidget *widget) const
-{
-    int ret = 0;
-
-    switch (m) {
-        //    case PM_ProgressBarChunkWidth:
-        //        ret = 9;
-        //        break;
-    default:
-        QProxyStyle::pixelMetric (m, opt, widget);
-        break;
-    }
-
-    return ret;
-}
-
-void ProgressBarStyle::drawPrimitive(PrimitiveElement elem, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
-{
-    switch (elem) {
-        //    case PE_IndicatorProgressChunk: {
-        //        bool vertical = false;
-        //        if (const QStyleOptionProgressBar *pb = qstyleoption_cast<const QStyleOptionProgressBar *>(option)) {
-        //            vertical = !(pb->state & QStyle::State_Horizontal);
-        //        }
-        //        if (!vertical) {
-        //            painter->fillRect(option->rect.x(), option->rect.y() + 3, option->rect.width() -2, option->rect.height() - 6, option->palette.brush(QPalette::Highlight));
-        //        } else {
-        //            painter->fillRect(option->rect.x() + 2, option->rect.y(), option->rect.width() -6, option->rect.height() - 2, option->palette.brush(QPalette::Highlight));
-        //        }
-        //    }
-        //    break;
-    default:
-        QProxyStyle::drawPrimitive (elem, option, painter, widget);
-        break;
-    }
-}
-
-void ProgressBarStyle::drawControl(ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
-{
-    QRect rect = option->rect;
-    QColor highlight = option->palette.color (QPalette::Highlight);
-
-    switch (element) {
-    case CE_ProgressBar:
-        if (const QStyleOptionProgressBar *pb = qstyleoption_cast<const QStyleOptionProgressBar *>(option)) {
-            QStyleOptionProgressBar subopt = *pb;
-            subopt.rect = QProxyStyle::subElementRect(SE_ProgressBarGroove, pb, widget);
-
-            // 外框和背景
-            QProxyStyle::drawControl(CE_ProgressBarGroove, &subopt, painter, widget);
-
-            // 修改这里
-            subopt.rect = QProxyStyle::subElementRect(SE_ProgressBarContents, pb, widget);
-
-            drawControl(CE_ProgressBarContents, &subopt, painter, widget);
-            if (pb->textVisible) {
-                subopt.rect = QProxyStyle::subElementRect(SE_ProgressBarLabel, pb, widget);
-                QProxyStyle::drawControl(CE_ProgressBarLabel, &subopt, painter, widget);
-            }
-        }
-        break;
-    case CE_ProgressBarGroove:
-        if (option->rect.isValid()) {
-            const bool enable = option->state & State_Enabled;
-            painter->save();
-            painter->setPen(Qt::NoPen);
-            painter->setBrush(option->palette.brush(enable ? QPalette::Active : QPalette::Disabled, QPalette::Button));
-            painter->drawRoundedRect(option->rect, 4, 4);
-            painter->restore();
-        }
-        break;
-    case CE_ProgressBarContents:
-        painter->save();
-        painter->setRenderHint(QPainter::Antialiasing, true);
-        painter->translate(0.5, 0.5);
-        if (const QStyleOptionProgressBar *bar = qstyleoption_cast<const QStyleOptionProgressBar *>(option)) {
-            bool vertical = false;
-            bool inverted = false;
-            bool indeterminate = (bar->minimum == 0 && bar->maximum == 0);
-            bool complete = bar->progress == bar->maximum;
-
-            vertical = !(bar->state & QStyle::State_Horizontal);
-            inverted = bar->invertedAppearance;
-            if (vertical) {
-                rect = QRect(rect.left(), rect.top(), rect.height(), rect.width());
-                QTransform m = QTransform::fromTranslate(rect.height()-1, -1.0);
-                m.rotate(90.0);
-                painter->setTransform(m, true);
-            }
-
-            int maxWidth = rect.width();
-            const auto progress = qMax(bar->progress, bar->minimum);
-            const auto totalSteps = qMax(Q_INT64_C(1), qint64(bar->maximum) - bar->minimum);
-            const auto progressSteps = qint64(progress) - bar->minimum;
-            const auto progressBarWidth = progressSteps * maxWidth / totalSteps;
-            int width = indeterminate ? maxWidth : progressBarWidth;
-
-            bool reverse = (!vertical && (bar->direction == Qt::RightToLeft)) || vertical;
-            if (inverted) {
-                reverse = !reverse;
-            }
-
-            float step = 0;
-            if (indeterminate || bar->progress > bar->minimum) {
-                // 等待状态的进度条
-                if (indeterminate) {
-                    int ww = width / 5 * 2;
-                    int endPoint = (rect.width () - ww);
-                    painter->setBrush (QBrush(QPalette().color(QPalette::Active, QPalette::Highlight)));
-                    if (QAbstractAnimation::Running == mAnimation->state ()) {
-                        step = qobject_cast<QVariantAnimation*>(mAnimation)->currentValue ().toFloat () / 100 * endPoint * 2;
-                        step = (step >= endPoint) ? (2 * endPoint - step) : step;
-                    } else {
-                        mAnimation->stop ();
-                        mAnimation->start ();
-                    }
-                    painter->drawRect (step, 0, ww, rect.height ());
-                } else {
-                    if (QAbstractAnimation::Running == mAnimation->state ()) {
-                        mAnimation->stop ();
-                    }
-
-                    if (bar->progress > bar->minimum) {
-                        QProxyStyle::drawControl(CE_ProgressBarContents, option, painter, widget);
-                    }
-                }
-            }
-        }
-        painter->restore();
-        break;
-    case CE_ProgressBarLabel:
-        //        if (const QStyleOptionProgressBar *pb = qstyleoption_cast<const QStyleOptionProgressBar *>(option)) {
-        //            const bool vertical = !(pb->state & QStyle::State_Horizontal);
-        //            if (!vertical) {
-        //                QPalette::ColorRole textRole = QPalette::NoRole;
-        //                if ((pb->textAlignment & Qt::AlignCenter) && pb->textVisible && ((qint64(pb->progress) - qint64(pb->minimum)) * 2 >= (qint64(pb->maximum) - qint64(pb->minimum)))) {
-        //                    textRole = QPalette::HighlightedText;
-        //                    //Draw text shadow, This will increase readability when the background of same color
-        //                    QRect shadowRect(pb->rect);
-        //                    shadowRect.translate(1,1);
-        //                    QColor shadowColor = (pb->palette.color(textRole).value() <= 128) ? QColor(255,255,255,160) : QColor(0,0,0,160);
-        //                    QPalette shadowPalette = pb->palette;
-        //                    shadowPalette.setColor(textRole, shadowColor);
-        //                    QProxyStyle:: drawItemText(painter, shadowRect, Qt::AlignCenter | Qt::TextSingleLine, shadowPalette, pb->state & State_Enabled, pb->text, textRole);
-        //                }
-        //                QProxyStyle::drawItemText(painter, pb->rect, Qt::AlignCenter | Qt::TextSingleLine, pb->palette, pb->state & State_Enabled, pb->text, textRole);
-        //            }
-        //        }
-        break;
-    default:
-        QProxyStyle::drawControl (element, option, painter, widget);
-        break;
-    }
-}
-
-QRect ProgressBarStyle::subElementRect(SubElement sr, const QStyleOption *opt, const QWidget *widget) const
-{
-    QRect r;
-    switch (sr) {
-        //    case SE_ProgressBarLabel:
-        //    case SE_ProgressBarGroove:
-        //    case SE_ProgressBarContents:
-        //        if (const QStyleOptionProgressBar *pb = qstyleoption_cast<const QStyleOptionProgressBar *>(opt)) {
-        //            int textw = 0;
-        //            const bool vertical = !(pb->state & QStyle::State_Horizontal);
-        //            if (!vertical) {
-        //                if (pb->textVisible) {
-        //                    textw = qMax(pb->fontMetrics.horizontalAdvance(pb->text), pb->fontMetrics.horizontalAdvance(QLatin1String("100%"))) + 6;
-        //                }
-        //            }
-
-        //            if ((pb->textAlignment & Qt::AlignCenter) == 0) {
-        //                if (sr != SE_ProgressBarLabel) {
-        //                    r.setCoords(pb->rect.left(), pb->rect.top(), pb->rect.right() - textw, pb->rect.bottom());
-        //                } else {
-        //                    r.setCoords(pb->rect.right() - textw, pb->rect.top(), pb->rect.right(), pb->rect.bottom());
-        //                }
-        //            } else {
-        //                r = pb->rect;
-        //            }
-        //            r = visualRect(pb->direction, pb->rect, r);
-        //        }
-        //        break;
-    default:
-        r = QProxyStyle::subElementRect(SE_ProgressBarGroove, opt, widget);
-        break;
-    }
-
-    return r;
-}
-
-ProgressBarStyle::ProgressBarStyle()
-{
-    mAnimation = new QVariantAnimation (this);
-    mAnimation->setStartValue (0);
-    mAnimation->setEndValue (100);
-    mAnimation->setDuration (1000);
-}
-
-ProgressBarStyle::~ProgressBarStyle()
-{
-    delete mAnimation;
 }
