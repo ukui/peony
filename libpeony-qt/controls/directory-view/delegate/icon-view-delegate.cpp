ukui/peony

commit b2a2a2b104c6cc329b9a5b678a037fc3a9a02915

[FIX] #92197 #90810 view dirty region issue.
note that desktop icon view grid size should be modified for adapting
new sizeHint() of delegate.

libpeony-qt/controls/directory-view/delegate/icon-view-delegate.cpp
@@ -75,10 +75,9 @@ QSize IconViewDelegate::sizeHint(const QStyleOptionViewItem &option, const QMode
 
     auto view = qobject_cast<IconView*>(this->parent());
     auto iconSize = view->iconSize();
-    auto font = qApp->font();
-    auto fm = QFontMetrics(font);
+    auto fm = qApp->fontMetrics();
     int width = iconSize.width() + 41;
-    int height = iconSize.height() + fm.ascent()*2 + 20;
+    int height = iconSize.height() + fm.ascent()*2 + 20 + 10;
     return QSize(width, height);
     /*
     qDebug()<<option;

peony-qt-desktop/desktop-icon-view-delegate.cpp
@@ -151,6 +151,8 @@ void DesktopIconViewDelegate::paint(QPainter *painter, const QStyleOptionViewIte
     style->drawControl(QStyle::CE_ItemViewItem, &opt, painter, opt.widget);
 
     opt.text = text;
+    opt.font = qApp->font();
+    opt.fontMetrics = qApp->fontMetrics();
 
     painter->save();
     //painter->translate(visualRect.topLeft());
@@ -313,20 +315,18 @@ void DesktopIconViewDelegate::paint(QPainter *painter, const QStyleOptionViewIte
 
 QSize DesktopIconViewDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    auto view = qobject_cast<Peony::DesktopIconView*>(parent());
-    auto zoomLevel = view->zoomLevel();
-    switch (zoomLevel) {
-    case DesktopIconView::Small:
-        return QSize(60, 70);
-    case DesktopIconView::Normal:
-        return QSize(90, 100);
-    case DesktopIconView::Large:
-        return QSize(105, 128);
-    case DesktopIconView::Huge:
-        return QSize(120, 150);
-    default:
-        return QSize(90, 100);
-    }
+    QStyleOptionViewItem opt = option;
+    initStyleOption(&opt, index);
+
+    auto view = qobject_cast<DesktopIconView*>(this->parent());
+    auto iconSize = view->iconSize();
+    auto font = view->font();
+    // asume max text size.
+    font.setPointSize(15);
+    auto fm = QFontMetrics(font);
+    int width = iconSize.width() + 41;
+    int height = iconSize.height() + fm.ascent()*2 + 20;
+    return QSize(width, height);
 }
 
 QWidget *DesktopIconViewDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const

peony-qt-desktop/desktop-icon-view.cpp
@@ -1561,20 +1561,20 @@ void DesktopIconView::setDefaultZoomLevel(ZoomLevel level)
     switch (level) {
     case Small:
         setIconSize(QSize(24, 24));
-        setGridSize(QSize(64, 74));
+        setGridSize(QSize(5, 5) + itemDelegate()->sizeHint(viewOptions(), QModelIndex()));
         break;
     case Large:
         setIconSize(QSize(64, 64));
-        setGridSize(QSize(115, 145));
+        setGridSize(QSize(15, 15) + itemDelegate()->sizeHint(viewOptions(), QModelIndex()));
         break;
     case Huge:
         setIconSize(QSize(96, 96));
-        setGridSize(QSize(140, 180));
+        setGridSize(QSize(20, 20) + itemDelegate()->sizeHint(viewOptions(), QModelIndex()));
         break;
     default:
         m_zoom_level = Normal;
         setIconSize(QSize(48, 48));
-        setGridSize(QSize(96, 106));
+        setGridSize(QSize(10, 10) + itemDelegate()->sizeHint(viewOptions(), QModelIndex()));
         break;
     }
     clearAllIndexWidgets();
@@ -2116,6 +2116,10 @@ QRect DesktopIconView::visualRect(const QModelIndex &index) const
         break;
     }
     rect.moveTo(rect.topLeft() + p);
+
+    auto size = itemDelegate()->sizeHint(QStyleOptionViewItem(), index);
+    rect.setSize(size);
+
     return rect;
 }
 

peony-qt-desktop/desktop-index-widget.cpp
@@ -147,7 +147,7 @@ void DesktopIndexWidget::paintEvent(QPaintEvent *e)
     pixmap.fill(Qt::transparent);
     QPainter shadowPainter(&pixmap);
     shadowPainter.setPen(Qt::black);
-    Peony::DirectoryView::IconViewTextHelper::paintText(&shadowPainter, m_option, m_index, maxTextHight, 2, 0, false, Qt::black);
+    Peony::DirectoryView::IconViewTextHelper::paintText(&shadowPainter, m_option, m_index, maxTextHight, 2, 4, false, Qt::black);
     shadowPainter.end();
 
     QImage shadowImage(expectedSize + QSize(4, 4), QImage::Format_ARGB32_Premultiplied);
@@ -175,10 +175,11 @@ void DesktopIndexWidget::paintEvent(QPaintEvent *e)
 
     // draw text
     p.setPen(m_option.palette.highlightedText().color());
+    p.setFont(qApp->font());
     Peony::DirectoryView::IconViewTextHelper::paintText(&p,
             m_option,
             m_index,
-            9999,
+            maxTextHight,
             2,
             4,
             false);

commit 0f193756b98215b6ae1e0794b430b6fc526da759

IconView: fix wrong grid size while switch view type form list view to icon view caused by b2a2a2b104c6cc329b9a5b678a037fc3a9a02915.
link to: #90810.

libpeony-qt/controls/directory-view/delegate/icon-view-delegate.cpp
@@ -70,8 +70,8 @@ IconViewDelegate::~IconViewDelegate()
 
 QSize IconViewDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    QStyleOptionViewItem opt = option;
-    initStyleOption(&opt, index);
+    //QStyleOptionViewItem opt = option;
+    //initStyleOption(&opt, index);
 
     auto view = qobject_cast<IconView*>(this->parent());
     auto iconSize = view->iconSize();

libpeony-qt/controls/directory-view/view/icon-view/icon-view.cpp
@@ -101,8 +101,8 @@ IconView::IconView(QWidget *parent) : QListView(parent)
 
     setContextMenuPolicy(Qt::CustomContextMenu);
 
-    setGridSize(QSize(115, 135));
     setIconSize(QSize(64, 64));
+    setGridSize(itemDelegate()->sizeHint(QStyleOptionViewItem(), QModelIndex()) + QSize(20, 20));
 
     m_renameTimer = new QTimer(this);
     m_renameTimer->setInterval(3000);