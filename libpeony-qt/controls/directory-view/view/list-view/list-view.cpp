fb3f36e5c4a03d237c5dece31fc57c0014b5c214
niceboy <niceboy@niceboy-vmware-virtual-platform.localdomain>
2021-12-17 16:54:12 +0800

    KYLIN:FIX THE BUG 82790;
     -BUG号：82790；
     -其他改动说明：禁用在“filesafe:///”路径、“file:///home/user/.box”和“file:///box/user”路径下，tab页图标模式和列表模式的鼠标拖拽功能；
    
    Signed-off-by: niceboy <niceboy@niceboy-vmware-virtual-platform.localdomain>

--- libpeony-qt/controls/directory-view/view/list-view/list-view.cpp
+++ libpeony-qt/controls/directory-view/view/list-view/list-view.cpp
@@ -57,6 +57,8 @@
 
 #include <QStyleOptionViewItem>
 
+#include <QStandardPaths>
+
 using namespace Peony;
 using namespace Peony::DirectoryView;
 
@@ -694,6 +696,17 @@ const QString ListView::getDirectoryUri()
 void ListView::setDirectoryUri(const QString &uri)
 {
     m_current_uri = uri;
+
+    QString username = QStandardPaths::writableLocation(QStandardPaths::HomeLocation).split("/").last();
+
+    QString boxpath = "file://"+QStandardPaths::writableLocation(QStandardPaths::HomeLocation)+"/.box";
+    QString oldboxpath = "file://box/"+username;
+
+    if(uri == "filesafe:///" || uri == boxpath || uri == oldboxpath){
+        setDragEnabled(false);
+    }else {
+        setDragEnabled(true);
+    }
 }
 
 const QStringList ListView::getSelections()
