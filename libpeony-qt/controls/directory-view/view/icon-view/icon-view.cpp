8fce7d6cb695a7409d4bf8066bd01857f2e1c072
hemh <hemeihong@kylinos.cn>
2021-12-17 17:42:21 +0800

    处理方式待商榷，建议是拖动行为无效，而不是禁用用户行为。
    Revert "KYLIN:FIX THE BUG 82790;"
    
    This reverts commit fb3f36e5c4a03d237c5dece31fc57c0014b5c214.

--- libpeony-qt/controls/directory-view/view/icon-view/icon-view.cpp
+++ libpeony-qt/controls/directory-view/view/icon-view/icon-view.cpp
@@ -60,8 +60,6 @@
 #include <QDebug>
 #include <QToolTip>
 
-#include <QStandardPaths>
-
 using namespace Peony;
 using namespace Peony::DirectoryView;
 
@@ -182,17 +180,6 @@ void IconView::setCutFiles(const QStringList &uris)
 void IconView::setDirectoryUri(const QString &uri)
 {
     m_current_uri = uri;
-
-    QString username = QStandardPaths::writableLocation(QStandardPaths::HomeLocation).split("/").last();
-
-    QString boxpath = "file://"+QStandardPaths::writableLocation(QStandardPaths::HomeLocation)+"/.box";
-    QString oldboxpath = "file://box/"+username;
-
-    if(uri == "filesafe:///" || uri == boxpath || uri == oldboxpath){
-        setMovement(QListView::Static);
-    }else {
-        setMovement(QListView::Snap);
-    }
 }
 
 const QString IconView::getDirectoryUri()
