18c61d9413f71f660b1f82455aa485d410450c72
hewenfei <hewenfei@kylinos.cn>
2022-02-16 16:43:39 +0800

    Update the copyright information of the desktop module

--- peony-qt-desktop/desktops/tablet/src/UtilityFunction/toolbox.cpp
+++ peony-qt-desktop/desktops/tablet/src/UtilityFunction/toolbox.cpp
@@ -1,3 +1,23 @@
+/*
+ * Peony-Qt
+ *
+ * Copyright (C) 2021, KylinSoft Co., Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "toolbox.h"
 #include <QVBoxLayout>
 #include <QDebug>
@@ -39,7 +59,7 @@ ToolBox::ToolBox(QWidget *parent,int w,int h) : QWidget(parent)
     });
     timeSetting=new QGSettings(TIME_FORMAT);
     connect(timeSetting,&QGSettings::changed,this, [=](const QString &key)
-    {      
+    {
          if(key=="hoursystem")
          {
              hourLabel->setText(Time->currentTime.split(":").first());
