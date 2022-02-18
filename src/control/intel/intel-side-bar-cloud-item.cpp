5cd0fc98976b848dc142367e3a36324a1bda8a35
Yue-Lan <lanyue@ubuntukylin.com>
2022-02-18 20:30:16 +0800

    add missing copyright header

--- src/control/intel/intel-side-bar-cloud-item.cpp
+++ src/control/intel/intel-side-bar-cloud-item.cpp
@@ -1,6 +1,26 @@
-        #include "intel-side-bar-cloud-item.h"
+/*
+ * Peony-Qt's Library
+ *
+ * Copyright (C) 2020, KylinSoft Co., Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: Wang Weinan <wangweinan@kylinos.cn>
+ *
+ */
 
-
+#include "intel-side-bar-cloud-item.h"
 #include "intel-side-bar-model.h"
 #include "file-utils.h"
 #include <QStandardPaths>
