e94ed53a22931b82dde9793d80b03a08dc3f28b8
Qxiangwenjie <xiangwenjie@kylinos.cn>
2022-03-28 11:22:46 +0800

    更新翻译

--- translations/peony-qt-desktop/peony-qt-desktop_zh_CN.ts
+++ translations/peony-qt-desktop/peony-qt-desktop_zh_CN.ts
@@ -9,12 +9,12 @@
         <translation>桌面图标视图</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="572"/>
+        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="575"/>
         <source>New Folder</source>
         <translation>新建文件夹</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="664"/>
+        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="667"/>
         <source>set background</source>
         <translation>设置背景</translation>
     </message>
@@ -23,22 +23,22 @@
         <translation type="vanished">删除文件警告</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="896"/>
+        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="900"/>
         <source>Open failed</source>
         <translation>打开失败</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="897"/>
+        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="901"/>
         <source>Open directory failed, you have no permission!</source>
         <translation>打开文件夹失败，您没有该目录的权限！</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="880"/>
+        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="884"/>
         <source>Open Link failed</source>
         <translation>打开快捷方式失败</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="881"/>
+        <location filename="../../peony-qt-desktop/desktop-icon-view.cpp" line="885"/>
         <source>File not exist, do you want to delete the link file?</source>
         <translation>目标文件夹不存在，是否删除该无效快捷方式？</translation>
     </message>
@@ -399,22 +399,22 @@
         <translation type="vanished">桌面</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="368"/>
+        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="371"/>
         <source>Close the peony desktop window</source>
         <translation>关闭桌面程序</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="371"/>
+        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="374"/>
         <source>Take over the dbus service.</source>
         <translation>接管DBus服务。</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="374"/>
+        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="377"/>
         <source>Take over the desktop displaying</source>
         <translation>接管桌面</translation>
     </message>
     <message>
-        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="377"/>
+        <location filename="../../peony-qt-desktop/peony-desktop-application.cpp" line="380"/>
         <source>Setup backgrounds</source>
         <translation>Setup backgrounds</translation>
     </message>
